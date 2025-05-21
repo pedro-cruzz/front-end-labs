26a44e5fc375fd4dd2a751a7e1d9ab61d81f0769
pedro-cruzz <pedrocruzvb123@gmail.com>
2025-05-21 19:12:18 -0300

    introdução ao DOM

--- simple-html/mirror-fashion/script/main.js
+++ simple-html/mirror-fashion/script/main.js
@@ -1,2 +1,13 @@
 let nome = "Pedro";
 alert("Boa noite, " + nome);
+
+const linkExterno = 
+        document.querySelector('a[title="Google Maps"]');
+
+//linkExterno.onclick = () => {
+  //  alert("Você será redirecionado para um link externo.");
+//};
+
+linkExterno.addEventListener('click', () => {
+        alert("Você será redirecionado para um link externo.");
+});
